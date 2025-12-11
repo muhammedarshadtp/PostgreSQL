21c5ef055a511cd5fa6cc281cb9be7e8b033b212
muhammedarshadtp <arshadtp89@gmail.com>
2025-12-11 17:38:49 +0530

    first commit in postgresql

---
+++ my_queries.sql
@@ -0,0 +1,11 @@
+
+    
+            -- create table in postgresql
+
+-- CREATE TABLE bike(id SERIAL PRIMARY KEY, brand VARCHAR(50),model VARCHAR(50),year INT);
+
+-- SELECT * FROM bike;
+
+--          insert column in table
+INSERT INTO bike(brand,model,year) VALUES('Yamaha', 'MT-09', 2021);
+SELECT * FROM bike;
