0c4001b526392a68c04430694ce09b79fad56555
muhammedarshadtp <arshadtp89@gmail.com>
2025-12-11 17:55:04 +0530

    Add bike table updates: insert rows, edit data, and add new column

--- my_queries.sql
+++ my_queries.sql
@@ -6,6 +6,33 @@
 
 -- SELECT * FROM bike;
 
---          insert column in table
-INSERT INTO bike(brand,model,year) VALUES('Yamaha', 'MT-09', 2021);
-SELECT * FROM bike;
+--          insert ROWS in table
+-- INSERT INTO bike(brand,model,year) VALUES('Yamaha', 'MT-09', 2021);
+-- SELECT * FROM bike;
+
+--     INSERT MULTIPLY ROWS DATA
+
+-- INSERT INTO bike(brand,model,year) VALUES('KTM', 'Duke 390', 2023),
+-- ('Royal Enfield', 'Classic 350', 2022),
+-- ('Harley-Davidson', 'Iron 883', 2020),
+-- ('Triumph', 'Street Triple RS', 2021);
+
+-- SELECT * FROM bike;
+
+--           ADD COLUMN IN TABLE
+
+-- ALTER TABLE bike ADD color VARCHAR(225);
+
+-- SELECT * FROM bike;
+
+--             UPDATE ROWS IN TABLE(it effect all the rows datas)
+
+-- UPDATE bike SET color='red';
+
+-- SELECT * FROM bike;
+
+--           if we user WHERE specifically rows effect 
+
+-- UPDATE bike SET color='black' WHERE brand='KTM';
+
+-- SELECT * FROM bike;
